8aa98dcde81688a8e32152e5d68bc11ae3ef00a3
Guillermo Coro <coroguillermo@gmail.com>
2015-09-13 22:34:38 -0300

    fix cloudinary

--- config/initializers/spree_cloudinary.rb
+++ config/initializers/spree_cloudinary.rb
@@ -5,5 +5,4 @@ Cloudinary.config do |config|
   config.cloud_name = 'dzcm4exzv'
   config.api_key = '565562468574965'
   config.api_secret = 'mdYoglV9hxaaAuExo3oBhNabep8'
-  config.cdn_subdomain = true
 end
